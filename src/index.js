08bfb185dddf8b6a929d78b4f105be04460bff84
Subhadeep <subhadeepbanerjee480@gmail.com>
2024-11-28 16:01:16 +0530

    Testing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-console.log("hi");
+console.log("hello");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
