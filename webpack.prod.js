5f4536e9e6ca2f57efbc3e44f203e45084828573
Subhadeep <subhadeepbanerjee480@gmail.com>
2024-12-02 12:07:34 +0530

    Update webpack.prod.js to specify build output directory and improve cache handling

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,23 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require("path");
 
 process.env["NODE_ENV"] = "production";
 
-module.exports = merge([
-  common,
-  {
-    mode: "production",
-    optimization: {
-      minimize: true,
-      minimizer: [
-        // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-        // `...`,
-        new CssMinimizerPlugin(),
-      ],
-    },
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    // Set the output directory to 'build'
+    path: path.resolve(__dirname, "build"),
+    filename: "static/js/[name].[contenthash].js", // Set filename with contenthash for better cache handling
+    clean: true, // Clean the build folder before each build
   },
-]);
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // Use CssMinimizerPlugin for CSS minimization
+      new CssMinimizerPlugin(),
+    ],
+  },
+});
